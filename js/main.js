39f6f8b994dc88776f7a00354d16dde29f9fb671
Justin <justinh_12@protonmail.com>
2025-10-24 03:56:03 -0700

    improve responsive behavior and affix post title to top

--- js/main.js
+++ js/main.js
@@ -3,18 +3,6 @@ const location = document.location.href;
 
 bodyClasses.add("home-page");
 
-if (location.indexOf('item') > -1) {
-  bodyClasses.add("post-page")
-  bodyClasses.remove("home-page");
-
-  const newOpLink = document.querySelector(".subtext a[href^='user?']");
-  const newOps = document.querySelector(`a[href='${newOpLink.attributes.href.value}'`)
-
-  newOps.classList.add("op")
-  newOps.text = `${newOpLink.text} [OP]`
-
-}
-
 if (location.indexOf('reply') > -1) {
   bodyClasses.add("post-page")
   bodyClasses.remove("home-page");
@@ -62,44 +50,46 @@ const logoutUrl = loggedIn
     : "";
 
 const htmlHeader = `
-<div class="headerBar hnbar">
-  <div class="titleItem orange">
-    <a href="news">Hacker News</a>
-  </div>
-  <div class="titleItem">
-    <a href="active">Active</a>
-  </div>
-  <div class="titleItem">
-    <a href="newest">New</a>
-  </div>
-  <div class="titleItem">
-    <a href="front">Past</a>
-  </div>
-  <div class="titleItem">
-    <a href="newcomments">Comments</a>
-  </div>
-  <div class="titleItem">
-    <a href="ask">Ask</a>
-  </div>
-  <div class="titleItem">
-    <a href="show">Show</a>
-  </div>
-  <div class="titleItem">
-    <a href="jobs">Jobs</a>
-  </div>
-  <div class="titleItem">
-    <a href="submit">Submit</a>
-  </div>
-  ${loggedIn ? `
-    <div class="titleItem">
-      <a href=${loggedIn.attributes.href.value}>${loggedIn.outerText}</a>
+<div class="headerContainer">
+    <div class="headerBar hnbar">
+      <div class="titleItem orange">
+        <a href="news">Hacker News</a>
+      </div>
+      <div class="titleItem activePosts">
+        <a href="active">Active</a>
+      </div>
+      <div class="titleItem newPosts">
+        <a href="newest">New</a>
+      </div>
+      <div class="titleItem pastPosts">
+        <a href="front">Past</a>
+      </div>
+      <div class="titleItem ">
+        <a href="newcomments">Comments</a>
+      </div>
+      <div class="titleItem">
+        <a href="ask">Ask</a>
+      </div>
+      <div class="titleItem">
+        <a href="show">Show</a>
+      </div>
+      <div class="titleItem">
+        <a href="jobs">Jobs</a>
+      </div>
+      <div class="titleItem">
+        <a href="submit">Submit</a>
+      </div>
+      ${loggedIn ? `
+        <div class="titleItem">
+          <a href=${loggedIn.attributes.href.value}>${loggedIn.outerText}</a>
+        </div>
+        <div class="titleItem">
+          <a href=${logoutUrl}>Logout</a>
+        </div>`
+        :`<div class="titleItem">
+          <a href="login?goto=news">Login</a>
+        </div>`}
     </div>
-    <div class="titleItem">
-      <a href=${logoutUrl}>Logout</a>
-    </div>`
-    :`<div class="titleItem">
-      <a href="login?goto=news">Login</a>
-    </div>`}
 </div>`
 
 const borderColors = [
@@ -113,17 +103,17 @@ const borderColors = [
   "Indigo"
 ]
 
-if(document.querySelector(".hnbar") == null) {
+if (document.querySelector(".hnbar") == null) {
 
   const newHeader = new DOMParser().parseFromString(htmlHeader, "text/html").body.children[0];
   document.querySelector("body > center:nth-child(1)").firstChild.before(newHeader)
 
-  if(document.documentURI.endsWith("news")) {
+  if (document.documentURI.endsWith("news")) {
     document.querySelectorAll("body > center > table > tbody > tr:nth-child(n + 2) > td > table tr:nth-child(3n + 2) td")
         .forEach(i => i.classList.add("hnHomepage"))
   }
 
-  else if(document.documentURI.includes("item")) {
+  else if (document.documentURI.includes("item")) {
     document.querySelectorAll("td[indent]")
         .forEach(i => {
           const indent = Number(i.attributes["indent"].value);
@@ -135,3 +125,98 @@ if(document.querySelector(".hnbar") == null) {
   }
 }
 
+if (location.indexOf('item') > -1) {
+    handlePostPage();
+}
+
+function handlePostPage() {
+    bodyClasses.add("post-page")
+    bodyClasses.remove("home-page");
+
+    const newOpLink = document.querySelector(".subtext a[href^='user?']");
+    const newOps = document.querySelector(`a[href='${newOpLink.attributes.href.value}'`)
+
+    newOps.classList.add("op")
+    newOps.text = `${newOpLink.text} [OP]`
+
+    affixPostInfo();
+}
+
+function affixPostInfo() {
+    const postDetailsTable = document.querySelector(".fatitem");
+
+    const upVoteLink = postDetailsTable.querySelector(".votelinks a").href;
+
+    const titleDetails = postDetailsTable.querySelector(".titleline > a");
+    const title = titleDetails.textContent;
+    const titleUrl = titleDetails.href;
+
+    const sublineDetails = postDetailsTable.querySelector(".subline");
+
+    const points = sublineDetails.querySelector(".score").textContent;
+
+    const postAuthorDetails = sublineDetails.querySelector(".op");
+    const postAuthorName = postAuthorDetails.textContent.split(" ")[0];
+    const postAuthorUrl = postAuthorDetails.href;
+
+    const postDateString = sublineDetails.querySelector(".age").title;
+    const postDate = Number.parseInt(postDateString.split(" ")[1]) * 1000;
+    const correctDate = new Date(postDate);
+    const dateDiffToNowHours = Math.round((new Date() - correctDate) / 3_600_000);
+
+    const age = dateDiffToNowHours > 12 ? correctDate.toDateString() : `${dateDiffToNowHours} hours ago`;
+
+    const links = sublineDetails.querySelectorAll("a");
+
+    const linksDict = {};
+
+    if (links.length >= 3) {
+        linksDict["hide"] = links[2]
+    }
+
+    if (links.length >= 4) {
+        linksDict["past"] = links[3]
+    }
+
+    if (links.length >= 5) {
+        linksDict["fav"] = links[4];
+    }
+
+    if (links.length >= 6) {
+        linksDict["commentsCount"] = links[5].textContent;
+    }
+
+    const htmlString = `
+    <div class="postTitleContainer">
+        <div class="mainPostTitle">
+            <span>${title}</span>
+            <span>(
+            <a href="${titleUrl}"><span class="smallText orangeText">${titleDetails.host}</span></a>
+            )</span>
+        </div>
+        <div class="postTitle">
+            <button class="upvotePost"><a href="${upVoteLink}"><div class="votearrow" title="upvote"></div></a></button>
+            <span>
+                <span class="yellowText">${points} by</span>
+                <span class="orangeText">
+                    <a href="${postAuthorUrl}">
+                        <span>${postAuthorName}</span>
+                    </a>
+                </span>
+            </span>    
+            <span class="greyText">${age}</span>
+            <span>
+                ${linksDict["hide"] != null ? `<span class="whiteText">| <a href="${linksDict["hide"]}">hide</a></span>` : null}
+                ${linksDict["past"] != null ? `<span class="whiteText">| <a href="${linksDict["past"]}">past</a></span>` : null}
+                ${linksDict["fav"] != null ? `<span class="whiteText">| <a href="${linksDict["fav"]}">favorite</a> |</span>` : null}
+            </span>
+            ${linksDict["commentsCount"] != null ? `<span class="greyText">${linksDict["commentsCount"]}</span>` : null}
+        </div>
+    </div>
+    `
+
+    postDetailsTable.parentNode.removeChild(postDetailsTable);
+
+    const postTitleElement = new DOMParser().parseFromString(htmlString, "text/html").body.children[0];
+    document.querySelector(".headerContainer").lastChild.after(postTitleElement);
+}
